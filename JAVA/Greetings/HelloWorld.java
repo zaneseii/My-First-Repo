904efaa976ab8780759fdd670ad4841197d15dd8
zaneseii <louis.dejesus@clsu2.edu.ph>
2026-02-23 13:13:21 +0800

    java commit

---
+++ JAVA/Greetings/HelloWorld.java
@@ -0,0 +1,7 @@
+package Greetings;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+    }
+}
---
+++ JAVA/Hobby/Hobby.java
@@ -0,0 +1,9 @@
+package Hobby;
+
+public class Hobby {
+    public static void main(String[] args) {
+        System.out.println("My favorite hobby is going to the gym and working out.");
+        System.out.println("I do it 4 times a week.");
+        System.out.println("Working out is important to me because it helps me stay healthy and fit. I also love the grind and the challenge of pushing myself to lift heavier weights and improve my strength.");
+    }
+}
